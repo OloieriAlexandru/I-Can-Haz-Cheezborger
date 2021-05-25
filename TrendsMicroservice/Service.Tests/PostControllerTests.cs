947bc5d0940140f02ee89bd151b22659f39ca969
Cojocariu Alexandru <cojocariualexandru1@gmail.com>
2021-05-25 18:05:37 +0300

    added controller tests

---
+++ TrendsMicroservice/Service.Tests/PostControllerTests.cs
@@ -0,0 +1,94 @@
+﻿using BusinessLogic.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using Models.Posts;
+using Moq;
+using Service.Controllers;
+using System;
+using Xunit;
+
+namespace Service.Tests
+{
+    public class PostControllerTests
+    {
+        private readonly Mock<IPostBusinessLogic> postBusinessLogicMock;
+
+        private readonly PostController systemUnderTest;
+
+        public PostControllerTests() : base()
+        {
+            postBusinessLogicMock = new Mock<IPostBusinessLogic>();
+            systemUnderTest = new PostController(postBusinessLogicMock.Object);
+        }
+
+        [Fact]
+        public void GetById_ReturnsTrendById()
+        {
+            //Arrange
+            PostGetByIdDto post = new PostGetByIdDto
+            {
+                Id = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6")
+            };
+            postBusinessLogicMock.Setup(x => x.GetById(post.Id, null)).Returns(post);
+
+            //Act
+            // https://stackoverflow.com/questions/61453820/get-a-value-from-actionresultobject-in-a-asp-net-core-api-method
+            var result = (OkObjectResult)systemUnderTest.GetByIdUnauthorized(post.Id).Result;
+            PostGetByIdDto postGetByIdDto = (PostGetByIdDto)result.Value;
+
+            //Assert
+            Assert.True(post.Id.Equals(postGetByIdDto.Id));
+            Assert.True(result.StatusCode.Equals(200));
+        }
+
+        [Fact]
+        public void GetById_ReturnsNullIfPostDoesNotExist()
+        {
+            PostGetByIdDto post = new PostGetByIdDto
+            {
+                Id = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6")
+            };
+            postBusinessLogicMock.Setup(x => x.GetById(post.Id, null)).Returns(() => null);
+
+            var result = (NotFoundResult)systemUnderTest.GetByIdUnauthorized(post.Id).Result;
+
+            Assert.True(result.StatusCode.Equals(404));
+        }
+
+        [Fact]
+        public void Patch_UpdatesPost()
+        {
+            PostPatchDto post = new PostPatchDto
+            {
+                Id = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6")
+            };
+
+            systemUnderTest.Patch(post.Id, post);
+
+            postBusinessLogicMock.Verify(m => m.Patch(post), Times.Once);
+        }
+
+        [Fact]
+        public void Patch_ReturnsBadRequestIfPostDoesNotExist()
+        {
+            PostPatchDto post = new PostPatchDto
+            {
+                Id = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6")
+            };
+
+            var result = (BadRequestResult)systemUnderTest.Patch(Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa7"), post);
+
+            Assert.True(result.StatusCode.Equals(400));
+        }
+
+        [Fact]
+        public void Delete_CallsDeleteFromBusinessLogic()
+        {
+            Guid id = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6");
+
+            var result = (NoContentResult)systemUnderTest.Delete(id);
+
+            postBusinessLogicMock.Verify(m => m.Delete(id), Times.Once);
+            Assert.True(result.StatusCode.Equals(204));
+        }
+    }
+}
--- TrendsMicroservice/Service.Tests/TrendsControllerTests.cs
+++ TrendsMicroservice/Service.Tests/TrendsControllerTests.cs
@@ -1,21 +1,97 @@
 ﻿using BusinessLogic.Abstractions;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using Models.Trends;
 using Moq;
 using Service.Controllers;
+using System;
+using Xunit;
 
 namespace Service.Tests
 {
-    [TestClass]
     public class TrendsControllerTests
     {
+
         private readonly Mock<ITrendBusinessLogic> trendBusinessLogicMock;
 
         private readonly TrendController systemUnderTest;
 
-        public TrendsControllerTests()
+
+        public TrendsControllerTests() : base()
         {
             trendBusinessLogicMock = new Mock<ITrendBusinessLogic>();
             systemUnderTest = new TrendController(trendBusinessLogicMock.Object);
+        }
+
+        [Fact]
+        public void GetById_ReturnsTrendById()
+        {
+            //Arrange
+            TrendGetByIdDto trend = new TrendGetByIdDto
+            {
+                Id = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6")
+            };
+            trendBusinessLogicMock.Setup(x => x.GetById(trend.Id)).Returns(trend);
+
+            //Act
+            // https://stackoverflow.com/questions/61453820/get-a-value-from-actionresultobject-in-a-asp-net-core-api-method
+            var result = (OkObjectResult)systemUnderTest.GetById(trend.Id).Result;
+            TrendGetByIdDto trendGetByIdDto = (TrendGetByIdDto)result.Value;
+
+            //Assert
+            Assert.True(trend.Id.Equals(trendGetByIdDto.Id));
+            Assert.True(result.StatusCode.Equals(200));
+        }
+
+        [Fact]
+        public void GetById_ReturnsNotFoundIfTrendDoesNotExist()
+        {
+            TrendGetByIdDto trend = new TrendGetByIdDto
+            {
+                Id = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6")
+            };
+            trendBusinessLogicMock.Setup(x => x.GetById(trend.Id)).Returns(() => null);
+
+            var result = (NotFoundResult)systemUnderTest.GetById(trend.Id).Result;
+
+            Assert.True(result.StatusCode.Equals(404));
+        }
+
+        [Fact]
+        public void Update_CallsUpdateFromBusinessLogic()
+        {
+            TrendUpdateDto trend = new TrendUpdateDto
+            {
+                Id = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6")
+            };
+
+            systemUnderTest.Update(Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6"), trend);
+
+            trendBusinessLogicMock.Verify(m => m.Update(trend), Times.Once);
+        }
+
+        [Fact]
+        public void Update_ReturnsBadRequestIfTrendDoesNotExist()
+        {
+            TrendUpdateDto trend = new TrendUpdateDto
+            {
+                Id = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6")
+            };
+
+            systemUnderTest.Update(Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa8"), trend);
+            var result = (BadRequestResult)systemUnderTest.Update(Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa8"), trend);
+
+            Assert.True(result.StatusCode.Equals(400));
+        }
+
+        [Fact]
+        public void Delete_CallsDeleteFromBusinessLogic()
+        {
+            Guid id = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6");
+
+            var result = (NoContentResult)systemUnderTest.Delete(id);
+
+            trendBusinessLogicMock.Verify(m => m.Delete(id), Times.Once);
+            Assert.True(result.StatusCode.Equals(204));
         }
     }
 }
